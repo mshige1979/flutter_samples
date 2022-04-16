2eb03533ba021b530cc8f4a8b742774bbe1b6f71
mshige1979 <mshige1979@ryuouen.net>
2022-04-17 08:42:51 +0900

    #3 drawer

--- 04_drawer/lib/screen/home.dart
+++ 04_drawer/lib/screen/home.dart
@@ -13,23 +13,60 @@ class _MyHomePageState extends State<MyHomePage> {
 
   @override
   Widget build(BuildContext context) {
-
     return Scaffold(
-      appBar: AppBar(
+        appBar: AppBar(
         title: Text(widget.title),
-      ),
-      body: Center(
-        child: Column(
-
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'ハンバーガーアイコン押下でドロワーメニューを開く',
+    ),
+    // 以下を追加するとAppBarにハンバーガーメニューが追加される
+    drawer: Drawer(
+        child: ListView(
+          children: [
+            ListTile(
+              title: Text('メニュー１'),
+              onTap: (){
+                debugPrint("xxxx");
+                Navigator.pop(context); //Drawerを閉じる
+              },
             ),
-
+            Divider(
+              thickness: 2,
+            ),
+            ListTile(
+              title: Text('メニュー2'),
+              onTap: (){
+                debugPrint("xxxx");
+                Navigator.pop(context); //Drawerを閉じる
+              },
+            ),
+            Divider(
+              thickness: 2,
+            ),
+            ListTile(
+              title: Text('メニュー3'),
+              onTap: (){
+                debugPrint("xxxx");
+                Navigator.pop(context); //Drawerを閉じる
+              },
+            ),
+            Divider(
+              thickness: 2,
+            ),
           ],
         ),
-      ),
+    ),
+    // body
+    body: Center(
+    child: Column(
+
+    mainAxisAlignment: MainAxisAlignment.center,
+    children: <Widget>[
+    const Text(
+    'ハンバーガーアイコン押下でドロワーメニューを開く',
+    ),
+
+    ],
+    ),
+    ),
     );
   }
 }
