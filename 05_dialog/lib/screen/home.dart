f40ca666130b354b1b6f46587f25a4dda49ddd44
mshige1979 <mshige1979@ryuouen.net>
2022-04-17 13:17:36 +0900

    #4 dialogのサンプルを作成

--- 05_dialog/lib/screen/home.dart
+++ 05_dialog/lib/screen/home.dart
@@ -13,6 +13,11 @@ class _MyHomePageState extends State<MyHomePage> {
 
   @override
   Widget build(BuildContext context) {
+
+    var _size = MediaQuery.of(context).size;
+    debugPrint("width : ${_size.width}, 70%: ${_size.width * 0.7}");
+    debugPrint("height: ${_size.height}");
+
     return Scaffold(
       appBar: AppBar(
         title: Text(widget.title),
@@ -23,6 +28,160 @@ class _MyHomePageState extends State<MyHomePage> {
           children: <Widget>[
             const Text(
               'ダイアログサンプル',
+            ),
+            Padding(
+              padding: EdgeInsets.all(10),
+              child: Column(
+                children: [
+                  OutlinedButton(
+                    style: OutlinedButton.styleFrom(minimumSize: Size(_size.width * 0.7, 40)),
+                    onPressed: () {
+                      debugPrint("load onPressed");
+
+                      // ダイアログ表示
+                      showDialog(
+                        context: context,
+                        builder: (context) {
+                          return SimpleDialog(
+                              title: Text("SimpleDialogタイトル"),
+                              children: <Widget>[
+                                // コンテンツ領域
+                                SimpleDialogOption(
+                                  onPressed: () => Navigator.pop(context),
+                                  child: Text("１項目目"),
+                                ),
+                                SimpleDialogOption(
+                                  onPressed: () => Navigator.pop(context),
+                                  child: Text("２項目目"),
+                                ),
+                                SimpleDialogOption(
+                                  onPressed: () => Navigator.pop(context),
+                                  child: Text("３項目目"),
+                                ),
+                                SimpleDialogOption(
+                                  onPressed: () => Navigator.pop(context),
+                                  child: Text("４項目目"),
+                                ),
+                                SimpleDialogOption(
+                                  onPressed: () => Navigator.pop(context),
+                                  child: Text("５項目目"),
+                                ),
+                              ],
+                          );
+                        },
+                      );
+                    },
+                    child: Text('SimpleDialog'),
+                  ),
+                ],
+              ),
+            ),
+            Padding(
+              padding: EdgeInsets.all(10),
+              child: Column(
+                children: [
+                  OutlinedButton(
+                    style: OutlinedButton.styleFrom(minimumSize: Size(_size.width * 0.7, 40)),
+                    onPressed: () {
+                      debugPrint("load onPressed");
+
+                      // アラートダイアログ用
+                      showDialog(
+                        context: context,
+                        builder: (_) {
+                          return AlertDialog(
+                            title: Text("AlertDialogタイトル"),
+                            content: Text("メッセージメッセージメッセージメッセージメッセージメッセージ"),
+                            actions: <Widget>[
+                              // ボタン領域
+                              TextButton(
+                                child: const Text("Cancel"),
+                                onPressed: () => Navigator.pop(context),
+                              ),
+                              TextButton(
+                                child: const Text("OK"),
+                                onPressed: () => Navigator.pop(context),
+                              ),
+                            ],
+                          );
+                        },
+                      );
+                    },
+                    child: Text('AlertDialog'),
+                  ),
+                ],
+              ),
+            ),
+            Padding(
+              padding: EdgeInsets.all(10),
+              child: Column(
+                children: [
+                  OutlinedButton(
+                    style: OutlinedButton.styleFrom(minimumSize: Size(_size.width * 0.7, 40)),
+                    onPressed: () {
+                      debugPrint("load onPressed");
+
+                      // アラートダイアログ用
+                      showDialog(
+                        context: context,
+                        builder: (context) => Column(
+                          children: <Widget>[
+                            AlertDialog(
+                              title: Text("AlertDialog(Custom)タイトル"),
+                              content: SingleChildScrollView(
+                                child: ListBody(
+                                  children: <Widget>[
+                                    Column(
+                                      children: [
+                                        const Text("aaaaaaaaaaaaaaa66666666666666666666666666666666666666666666666666666666aaa"),
+                                        const Text("aaaaaaaaaaaaaaaaaa"),
+                                        const Text("aaaaaaaaaaaaaaaaaa"),
+                                        const Text("aaaaaaaaaaaaaaaaaa"),
+                                        const Text("aaaaaaaaaaaaaaaaaa"),
+                                      ],
+                                      // コンテンツ
+                                    ),
+                                  ],
+                                ),
+                              ),
+                              actions: <Widget>[
+                                TextButton(
+                                  child: const Text("OK"),
+                                  onPressed: () => Navigator.pop(context),
+                                ),
+                              ],
+                            ),
+                          ],
+                        ),
+                      );
+                    },
+                    child: Text('AlertDialog(Custom)'),
+                  ),
+                ],
+              ),
+            ),
+            Padding(
+              padding: EdgeInsets.all(10),
+              child: Column(
+                children: [
+                  OutlinedButton(
+                    style: OutlinedButton.styleFrom(minimumSize: Size(_size.width * 0.7, 40)),
+                    onPressed: () {
+                      debugPrint("load onPressed");
+
+                      // アバウトダイアログ用
+                      showAboutDialog(
+                        context: context,
+                        applicationIcon: Icon(Icons.photo),
+                        applicationName: "アプリ名",
+                        applicationVersion: "2.0.1",
+                        applicationLegalese: "あいうえお",
+                      );
+                    },
+                    child: Text('AboutDialog'),
+                  ),
+                ],
+              ),
             ),
           ],
         ),
